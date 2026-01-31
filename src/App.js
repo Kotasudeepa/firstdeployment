a382d719104e56a014d005b73a4176896b67558e
kotasudeepa <sudeepakota@gmail.com>
2026-01-31 12:45:24 +0530

    code changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      This is first deployment project in vercel
+    </>
+  );
+}
+
+export default App;
